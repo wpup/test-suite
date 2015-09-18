c714cf86f4aa0f7794ab8d78a2a023cdfe20534c
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-09-19 00:04:35 +0200

    Add simple test

---
+++ tests/class-wp-test-suite-test.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Test to test so all works as it should.
+ */
+class WP_Test_Suite_Test extends WP_UnitTestCase {
+
+	public function wp_test() {
+		$this->assertTrue( true );
+	}
+
+}
